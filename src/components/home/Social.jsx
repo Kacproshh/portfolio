e09e87106a6e64758f50780184a9b9044bf322a8
unknown <kacprosh@icloud.com>
2025-01-26 12:04:38 +0100

    social links

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function Social() {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/kacprosh.12/" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="" className="home__social-icon" target="_blank">
-        <i className="uil uil-dribbble"></i>
+        <a href="https://www.linkedin.com/in/kacper-kulesza-316156344/" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin"></i>
         </a>
 
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="https://github.com/Kacproshh" className="home__social-icon" target="_blank">
         <i className="uil uil-github-alt"></i>
         </a>
     </div>
