1c7604c029ff421780905e9d6d68d88a144bd1d7
unknown <kacprosh@icloud.com>
2025-01-26 09:38:47 +0100

    home page update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 
 import React from 'react'
 import Header from './components/header/header';
+import Home from './components/home/Home'
 
 const App = () => {
   return (
     <>
     <Header />
+    <main className='main'>
+      <Home />
+    </main>
     </>
 
   )
